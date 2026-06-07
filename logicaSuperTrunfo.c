bd83169396c87a23f13d27f34002a3baa2c8a3a6
danlins128 <danlins8049@gmail.com>
2026-06-07 20:16:10 -0300

    Corrigindo Nível novato

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -70,6 +70,34 @@ int main() {
 
     percap1 = (double) pib1 * 1000000000.0 / populacao1;
     percap2 = (double) pib2 * 1000000000.0 / populacao2;
+
+    // Exibição das cartas
+
+    // Carta 01
+
+    printf("\nCarta 01\n");
+    printf("Estado: %c\n", estado1);
+    printf("Código: %s\n", codigo1);
+    printf("Nome da Cidade: %s\n", cidade1);
+    printf("População: %ld\n", populacao1);
+    printf("Área: %.2f km²\n", area1);
+    printf("PIB: %.2f bilhões de reais\n", pib1);
+    printf("Número de Pontos Turísticos: %d\n", pontos_turisticos1);
+    printf("Densidade populacional: %.2f hab/km²\n", densidade_populacional1);
+    printf("Pib per Capita: %.2f reais\n\n", percap1);
+
+    // Carta 02
+
+    printf("Carta 02\n");
+    printf("Estado: %c\n", estado2);
+    printf("Código: %s\n", codigo2);
+    printf("Nome da Cidade: %s\n", cidade2);
+    printf("População: %ld\n", populacao2);
+    printf("Área: %.2f km²\n", area2);
+    printf("PIB: %.2f bilhões de reais\n", pib2);
+    printf("Número de Pontos Turísticos: %d\n", pontos_turisticos2);    
+    printf("Densidade populacional: %.2f hab/km²\n", densidade_populacional2);
+    printf("Pib per Capita: %.2f reais\n\n", percap2);
     
     // Comparação de Cartas:
 
